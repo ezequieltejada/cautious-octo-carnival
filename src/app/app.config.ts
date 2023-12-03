c14509cc2181314e771fcafbaef19770020c3156
Ezequiel E. Tejada <et.github@outlook.com>
2023-12-03 13:36:41 +0000

    Add @ngrx/schematics and @ngrx/store-devtools dependencies

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,14 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideEffects()]
+  providers: [provideRouter(routes), provideStore({}), provideEffects([]), provideStoreDevtools({
+    maxAge: 25,
+    logOnly: isDevMode()
+  })]
 };
