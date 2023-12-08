48bc098646daae8193ba2bde2ef549ea4b0b2c77
Ezequiel E. Tejada <et.github@outlook.com>
2023-12-08 13:33:20 +0000

    Refactor favourites service to use BehaviorSubject

--- src/app/services/favourites/favourites.service.ts
+++ src/app/services/favourites/favourites.service.ts
@@ -2,17 +2,11 @@ import { Inject, Injectable, InjectionToken } from '@angular/core';
 import { Product, RawProduct } from '../../common/interfaces/product.interface';
 import { BehaviorSubject } from 'rxjs';
 
-// export const BROWSER_STORAGE = new InjectionToken<Storage>('Browser Storage', {
-//   providedIn: 'root',
-//   factory: () => localStorage
-// });
-
 @Injectable({
   providedIn: 'root'
 })
 export class FavouritesService {
 
-  // constructor(@Inject(BROWSER_STORAGE) public storage: Storage) { }
   private _favourites: BehaviorSubject<Product[]> = new BehaviorSubject<Product[]>([]);
 
   /**
@@ -23,12 +17,6 @@ export class FavouritesService {
    * @memberof FavouritesService
    */
   getFavourites(username: string): Product[] {
-    // const favouritesStr = this.storage.getItem(username);
-    // if (favouritesStr) {
-    //   return JSON.parse(favouritesStr);
-    // } else {
-    //   return [];
-    // }
     return this._favourites.getValue();
   }
 
@@ -42,15 +30,7 @@ export class FavouritesService {
    */
   addFavourite(username: string, product: any): Product[] {
     const favourites = this.getFavourites(username);
-    // if (favourites) {
-    //   favourites.push(product);
-    //   this.storage.setItem(username, JSON.stringify(favourites));
-    //   return favourites;
-    // } else {
-    //   this.storage.setItem(username, JSON.stringify([product]));
-    //   return [product];
-    // }
-    this._favourites.next([...favourites, product]);
+    this._favourites.next([...favourites, {...product, isFavourite: true}]);
     return this.getFavourites(username);
   }
 
